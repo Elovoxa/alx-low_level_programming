8b82928dc9e8f91a504070a1da4fda52b4d5674e
elovoxa <gnaouiy@gmail.com>
2023-03-21 11:35:22 -0700

    l

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,14 @@
-a = 1
-b = 2
-sum = 0
+int a = 1;
+int b = 2;
+int sum = 0;
 
-while b <= 4000000:
-    if b % 2 == 0:
-        sum += b
-    a, b = b, a + b
+while (b <= 4000000) {
+    if (b % 2 == 0) {
+        sum += b;
+    }
+    int temp = b;
+    b = a + b;
+    a = temp;
+}
 
-print(sum)
+printf("%d\n", sum);

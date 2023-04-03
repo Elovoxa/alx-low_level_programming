697e0786baf95a4f63d060bf6fb7cc437cd378fa
elovoxa <gnaouiy@gmail.com>
2023-04-03 07:16:00 -0700

    l

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
+ * _memcpy - function that copies memory area
+ *@dest: the address of memory to print
+ *@src: second value
+ *@n: thirth value
  *
- *  _memcpy - function that copies memory area
- *  @dest: the adress of memory to print
- *  @src: second value
- *  @n: third value
- *
- * Return: chat with result of memset
+ * Return: char with result of memset
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -15,5 +14,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-	}return (dest);
+	}
+	return (dest);
 }

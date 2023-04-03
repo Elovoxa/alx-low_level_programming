855dae07a129daf7ec42d1d18df28155ddb33193
elovoxa <gnaouiy@gmail.com>
2023-04-03 08:03:38 -0700

    l

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
-		for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar (a[j][i]);
+			_putchar (a[i][j]);
 		}
 		i_putchar('\n');
 	}

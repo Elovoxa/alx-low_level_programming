49fcc87c44aad841af24a1461909eae452b3d7dd
elovoxa <gnaouiy@gmail.com>
2023-04-11 10:48:17 -0700

    l

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,71 +2,46 @@
 #include <stdlib.h>
 
 /**
- * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
- * Return: 0
+ * argstostr - Concatenates all command line arguments into a single string
+ *
+ * @ac: Number of arguments
+ * @av: Array of pointers to arguments
+ *
+ * Return: Pointer to concatenated string on success, NULL on failure or if
+ *         either @ac or @av is 0
  */
-
 char *argstostr(int ac, char **av)
-
 {
-	int i, n, r = 0, l = 0;
-
+	int i, j, len = 0, total_len = 0;
 	char *str;
 
-
-
 	if (ac == 0 || av == NULL)
-
 		return (NULL);
 
-
-
 	for (i = 0; i < ac; i++)
-
 	{
-
-		for (n = 0; av[i][n]; n++)
-
-			l++;
-
+		for (j = 0; av[i][j]; j++)
+			len++;
+		total_len += len + 1;
+		len = 0;
 	}
 
-	l += ac;
-
-
-
-	str = malloc(sizeof(char) * l + 1);
-
+	str = malloc(sizeof(char) * total_len);
 	if (str == NULL)
-
 		return (NULL);
 
+	len = 0;
 	for (i = 0; i < ac; i++)
-
 	{
-
-	for (n = 0; av[i][n]; n++)
-
-	{
-
-		str[r] = av[i][n];
-
-		r++;
-
+		for (j = 0; av[i][j]; j++)
+		{
+			str[len] = av[i][j];
+			len++;
+		}
+		str[len] = '\n';
+		len++;
 	}
+	str[len] = '\0';
 
-	if (str[r] == '\0')
-
-	{
-
-		str[r++] = '\n';
-
-	}
-
-	}
-	
 	return (str);
-
 }

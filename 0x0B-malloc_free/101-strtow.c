7da306f6b02eb455e054f620a758e2f2818f4384
elovoxa <gnaouiy@gmail.com>
2023-04-11 16:37:11 -0700

    l

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,18 +9,18 @@
  */
 int count_word(char *s)
 {
-    int flag = 0, c = 0, w = 0;
+	int flag = 0, c = 0, w = 0;
 
-    for (c = 0; s[c] != '\0'; c++)
-    {
-        if (s[c] == ' ')
-            flag = 0;
-        else if (flag == 0)
-        {
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
             flag = 1;
             w++;
-        }
-    }
+		}
+	}
 
     return (w);
 }

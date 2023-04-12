9eeb5484c49fca4e4c9882563473d8e1e51373a1
elovoxa <gnaouiy@gmail.com>
2023-04-11 17:46:08 -0700

    l

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -26,50 +26,69 @@ int count_word(char *s)
 }
 
 /**
- * strtow - splits a string into words
- * @str: string to split
+ * copy_word - helper function to copy a word from a string
+ * @start: start index of the word
+ * @end: end index of the word
+ * @str: string to copy from
  *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: pointer to a newly-allocated copy of the word
  */
-char **strtow(char *str)
+char *copy_word(int start, int end, char *str)
 {
-	char **matrix, *tmp;
-	int i, k = 0, len = 0, words = 0, c = 0, start = 0, end = 0;
+	char *word = malloc(end - start + 1);
+	int i;
 
-	while (*(str + len))
-		len++;
-
-	words = count_word(str);
-
-	if (words == 0)
+	if (word == NULL)
 		return (NULL);
 
-	matrix = (char **)malloc(sizeof(char *) * (words + 1));
+	for (i = start; i < end; i++)
+		word[i - start] = str[i];
+
+	word[i - start] = '\0';
+
+	return (word);
+}
+
+/**
+ * allocate_matrix - helper function to allocate memory for the matrix
+ * @words: number of words in the string
+ *
+ * Return: pointer to a newly-allocated matrix
+ */
+char **allocate_matrix(int words)
+{
+	char **matrix;
+
+	matrix = malloc((words + 1) * sizeof(char *));
 
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = 0; i <= len; i++)
+	return (matrix);
+}
+
+/**
+ * fill_matrix - helper function to fill the matrix with words
+ * @str: string to split into words
+ * @matrix: matrix to fill
+ * @words: number of words in the string
+ */
+void fill_matrix(char *str, char **matrix, int words)
+{
+	int i, k = 0, c = 0, start = 0, end = 0;
+
+	for (i = 0; i <= words; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
 			if (c)
 			{
 				end = i;
-				tmp = (char *)malloc(sizeof(char) * (c + 1));
+				matrix[k] = copy_word(start, end, str);
 
-				if (tmp == NULL)
-					return (NULL);
+				if (matrix[k] == NULL)
+					return;
 
-				while (start < end)
-				{
-					*tmp++ = str[start++];
-				}
-
-				*tmp = '\0';
-
-				matrix[k] = tmp - c;
 				k++;
 				c = 0;
 			}
@@ -81,5 +100,37 @@ char **strtow(char *str)
 	}
 
 	matrix[k] = NULL;
+}
+
+/**
+ * strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix;
+	int len = 0, words = 0;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	while (*(str + len))
+		len++;
+
+	words = count_word(str);
+
+	if (words == 0)
+		return (NULL);
+
+	matrix = allocate_matrix(words);
+
+	if (matrix == NULL)
+		return (NULL);
+
+	fill_matrix(str, matrix, len);
+
 	return (matrix);
 }

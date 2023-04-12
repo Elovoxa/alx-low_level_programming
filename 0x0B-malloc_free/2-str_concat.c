5dc818b325c1438e0377b518a933d2f55a52fa38
elovoxa <gnaouiy@gmail.com>
2023-04-12 07:05:48 -0700

    l

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * alloc_grid - creates a 2D array of integers with given width and height
- * @width: width of the 2D array
- * @height: height of the 2D array
- *
- * Return: Pointer to the 2D array, or NULL if width or height is 0 or negative
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
-int **alloc_grid(int width, int height)
+char *str_concat(char *s1, char *s2)
 {
-	int **grid;
-	int i, j;
+	char *conct;
+	int i, ci;
 
-	if (width <= 0 || height <= 0)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
+
+	if (conct == NULL)
 		return (NULL);
-
-	grid = malloc(sizeof(int *) * height);
-	if (grid == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
+	i = ci = 0;
+	while (s1[i] != '\0')
 	{
-		grid[i] = malloc(sizeof(int) * width);
-		if (grid[i] == NULL)
-		{
-			for (j = i - 1; j >= 0; j--)
-				free(grid[j]);
-			free(grid);
-			return (NULL);
-		}
+		conct[i] = s1[i];
+		i++;
 	}
 
-	for (i = 0; i < height; i++)
+	while (s2[ci] != '\0')
 	{
-		for (j = 0; j < width; j++)
-			grid[i][j] = 0;
+		conct[i] = s2[ci];
+		i++, ci++;
 	}
-
-	return (grid);
+	conct[i] = '\0';
+	return (conct);
 }
